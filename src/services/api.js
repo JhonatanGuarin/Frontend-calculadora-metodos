9bd5d97e2988b0a653faacda01fc48b51bcb7b77
JhonatanGuarin <jhonatancamilo99@gmail.com>
2025-03-27 09:28:13 -0500

    Frond

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://api-gateway-9t7o.onrender.com/api";
 
 // Crear una instancia de axios con la URL base
 const api = axios.create({
